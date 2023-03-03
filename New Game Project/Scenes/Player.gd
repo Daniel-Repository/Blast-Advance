f7d02088d3abb87eca658a84a4234d80c5491d72
Daniel Carroll <danielcarrollbusiness@gmail.com>
2023-03-03 12:31:18 +0000

    Play area walls to keep player in

--- New Game Project/Scenes/Player.gd
+++ New Game Project/Scenes/Player.gd
@@ -8,20 +8,13 @@ var velocity = Vector2.ZERO
 func _ready() -> void:
 	pass # Replace with function body.
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	look_at(get_global_mouse_position())
 	get_input()
 
 func get_input():
 	if Input.is_action_just_pressed("ui_select"):
 		apply_central_impulse(position.direction_to(get_global_mouse_position()) * shoot_power)
-	#if Input.is_action_pressed("ui_up"):
-	#	velocity += transform.x * move_speed
-	#if Input.is_action_pressed("ui_down"):
-	#	velocity -= transform.x * move_speed
-	
-	
-	#velocity = velocity.normalized() * move_speed
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
